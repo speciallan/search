a7d85094fa1cb8663349b655606eea9f88f2118b
speciallan <350394776@qq.com>
2019-10-13 13:46:28 +0800

    test

--- scrapy/tutorial/tutorial/spiders/run.py
+++ scrapy/tutorial/tutorial/spiders/run.py
@@ -2,6 +2,26 @@
 # -*- coding:utf-8 -*-
 # Author:Speciallan
 
+import argparse
+import sys
 from scrapy.cmdline import execute
-# execute(['scrapy', 'crawl', 'product', "-a", "cate_id=1", "-a", "origin_id=1"])
-execute(['scrapy', 'crawl', 'comment'])
+
+
+def main(args):
+
+    if args.stage == 1:
+        execute(['scrapy', 'crawl', 'product', "-a", "cate_id=1", "-a", "origin_id=1"])
+    if args.stage == 2:
+        execute(['scrapy', 'crawl', 'comment'])
+
+    print('加入stage参数')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--stage', '-s', default=0, type=int, help='stage')
+    args = parser.parse_args(sys.argv[1:])
+
+    main(args)
+
+
